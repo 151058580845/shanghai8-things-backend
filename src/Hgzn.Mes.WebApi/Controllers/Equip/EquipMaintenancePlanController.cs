e07321da42497700d95787c3344a3c923dfb0d8a
GeJason <718420656@qq.com>
2025-01-24 09:05:19 +0800

    fix: 先提交一版

--- src/Hgzn.Mes.WebApi/Controllers/Equip/EquipMaintenancePlanController.cs
+++ src/Hgzn.Mes.WebApi/Controllers/Equip/EquipMaintenancePlanController.cs
@@ -1,4 +1,8 @@
-﻿using Hgzn.Mes.Application.Main.Services.Equip.IService;
+﻿using Hgzn.Mes.Application.Main.Dtos.Equip;
+using Hgzn.Mes.Application.Main.Services.Equip;
+using Hgzn.Mes.Application.Main.Services.Equip.IService;
+using Hgzn.Mes.Domain.Shared;
+using Hgzn.Mes.WebApi.Utilities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Hgzn.Mes.WebApi.Controllers.Equip
@@ -7,11 +11,18 @@ namespace Hgzn.Mes.WebApi.Controllers.Equip
     [ApiController]
     public class EquipMaintenancePlanController : ControllerBase
     {
-        private readonly IEquipMaintenancePlanService _equipMaintenancePlanService;
+        //private readonly IEquipMaintenancePlanService _equipMaintenancePlanService;
 
-        public EquipMaintenancePlanController(IEquipMaintenancePlanService equipMaintenancePlanService)
-        {
-            _equipMaintenancePlanService = equipMaintenancePlanService;
-        }
+        //public EquipMaintenancePlanController(IEquipMaintenancePlanService equipMaintenancePlanService)
+        //{
+        //    _equipMaintenancePlanService = equipMaintenancePlanService;
+        //}
+
+        //[HttpGet]
+        //[ProducesResponseType(StatusCodes.Status200OK)]
+        //[ProducesResponseType(StatusCodes.Status404NotFound)]
+        //[Route("list")]
+        //public async Task<ResponseWrapper<PaginatedList<EquipMaintenancePlanReadDto>>> GetPaginatedListAsync(EquipMaintenancePlanQueryDto queryDto)
+        //=> (await _equipItemsService.GetPaginatedListAsync(queryDto)).Wrap();
     }
 }
