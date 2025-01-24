ecadac98254a9d5c7ebbbe69500521367c83a78b
GeJason <718420656@qq.com>
2025-01-24 16:56:48 +0800

    feature:增删查改

--- src/Hgzn.Mes.WebApi/Controllers/Equip/EquipLedgerController.cs
+++ src/Hgzn.Mes.WebApi/Controllers/Equip/EquipLedgerController.cs
@@ -4,7 +4,9 @@ using Hgzn.Mes.Application.Main.Services.Equip;
 using Hgzn.Mes.Application.Main.Services.Equip.IService;
 using Hgzn.Mes.Domain.Entities.Equip.EquipData;
 using Hgzn.Mes.Domain.Shared;
+using Hgzn.Mes.Domain.ValueObjects;
 using Hgzn.Mes.WebApi.Utilities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using StackExchange.Redis;
 
@@ -60,5 +62,59 @@ namespace Hgzn.Mes.WebApi.Controllers.Equip
         //[Route("ImportEquipRooms")]
         //public async Task GetImportEquipRoomsAsync(EquipLedgerImportRoomInputVo data)
         //=> (await _equipLedgerService.GetImportEquipRoomsAsync(data)).Wrap();
+
+        /// <summary>
+        /// 创建
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("{id:guid}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [Authorize(Policy = $"system:code:{ScopeMethodType.Edit}")]
+        public async Task<ResponseWrapper<EquipLedgerReadDto?>> CreateAsync(EquipLedgerCreateDto input) =>
+            (await _equipLedgerService.CreateAsync(input)).Wrap();
+
+        /// <summary>
+        /// 删除
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpDelete]
+        [Route("{id:guid}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [Authorize(Policy = $"system:code:{ScopeMethodType.Remove}")]
+        public async Task<ResponseWrapper<int>> DeleteAsync(Guid id) =>
+            (await _equipLedgerService.DeleteAsync(id)).Wrap();
+
+        /// <summary>
+        /// 更新
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        [HttpPut]
+        [Route("{id:guid}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [Authorize(Policy = $"system:code:{ScopeMethodType.Edit}")]
+        public async Task<ResponseWrapper<EquipLedgerReadDto?>> UpdateAsync(Guid id, EquipLedgerUpdateDto input) =>
+            (await _equipLedgerService.UpdateAsync(id, input)).Wrap();
+
+        /// <summary>
+        /// 获取
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("{id:guid}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [Authorize(Policy = $"system:code:{ScopeMethodType.Add}")]
+        public async Task<ResponseWrapper<EquipLedgerReadDto?>> GetAsync(Guid id) =>
+            (await _equipLedgerService.GetAsync(id)).Wrap();
     }
 }
