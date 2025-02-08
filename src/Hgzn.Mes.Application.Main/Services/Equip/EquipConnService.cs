928e9c75e35717ccf2a9d183c0829ed3ac8d0b6e
GeJason <718420656@qq.com>
2025-02-08 11:25:27 +0800

    添加controller

--- src/Hgzn.Mes.Application.Main/Services/Equip/EquipConnService.cs
+++ src/Hgzn.Mes.Application.Main/Services/Equip/EquipConnService.cs
@@ -5,7 +5,12 @@ using Hgzn.Mes.Domain.Services;
 using Hgzn.Mes.Domain.Shared;
 using Hgzn.Mes.Domain.Shared.Enums;
 using Hgzn.Mes.Domain.Shared.Extensions;
+using Hgzn.Mes.Domain.ValueObjects.Message.Commads.Connections;
+using Hgzn.Mes.Infrastructure.Mqtt.Manager;
+using Hgzn.Mes.Infrastructure.Mqtt.Topic;
 using SqlSugar;
+using System.Text;
+using System.Text.Json;
 
 namespace Hgzn.Mes.Application.Main.Services.Equip;
 
@@ -16,10 +21,12 @@ public class EquipConnService : SugarCrudAppService<
     IEquipConnService
 {
     private readonly EquipLedgerService _equipLedgerService;
+    private readonly IMqttExplorer _mqttExplorer;
 
-    public EquipConnService(EquipLedgerService equipLedgerService)
+    public EquipConnService(EquipLedgerService equipLedgerService, IMqttExplorer mqttExplorer)
     {
         _equipLedgerService = equipLedgerService;
+        _mqttExplorer = mqttExplorer;
     }
 
 
@@ -77,8 +84,126 @@ public class EquipConnService : SugarCrudAppService<
         return await Task.FromResult(dots);
     }
 
-    public override Task<IEnumerable<EquipConnectReadDto>> GetListAsync(EquipConnectQueryDto? queryDto)
+    public async override Task<IEnumerable<EquipConnectReadDto>> GetListAsync(EquipConnectQueryDto? queryDto)
     {
-        throw new NotImplementedException();
+        var equips = await (await _equipLedgerService.GetEquipsListAsync(queryDto.EquipCode, queryDto.EquipName))
+            .OrderBy(t => t.OrderNum)
+            .ToListAsync();
+        var equipIds = equips.Select(t => t.Id).ToList();
+        if (equipIds.Count == 0)
+        {
+            return new List<EquipConnectReadDto>();
+        }
+
+        var query = Queryable
+            .Where(t => equipIds.Contains(t.EquipId));
+        RefAsync<int> total = await query.CountAsync();
+        var entities = await query
+            .Skip(queryDto.PageIndex)
+            .Take(queryDto.PageSize)
+            .Includes(t => t.ForwardEntities)
+            .Includes(t => t.EquipLedger, eq => eq!.EquipTypeAggregate)
+            .ToListAsync();
+        var outputs = await MapToGetListOutputDtosAsync(entities);
+        var equipDictionary = entities
+            .Select(t => t.EquipLedger)
+            .GroupBy(t => t!.Id)
+            .ToDictionary(g => g.Key, g => g.First()); // 获取每组的第一个实体;
+
+        foreach (EquipConnectReadDto outputDto in outputs)
+        {
+            if (equipDictionary.TryGetValue(outputDto.EquipId, out var entity))
+            {
+                outputDto.EquipCode = entity?.EquipCode;
+                outputDto.EquipName = entity?.EquipCode;
+                outputDto.TypeName = entity?.EquipTypeAggregate?.TypeName;
+                // 判断是否为 RFID 设备，并填充状态
+                if (outputDto.ProtocolEnum == Protocol.RfidReaderClient)
+                {
+                    outputDto.CollectionModel = outputDto.CollectionExtension switch
+                    {
+                        1 => "绑定Rfid标签",
+                        2 => "解绑Rfid标签",
+                        _ => "采集数据"
+                    };
+                }
+            }
+        }
+        return new List<EquipConnectReadDto>(outputs);
+    }
+
+    public async override Task<EquipConnectReadDto?> UpdateAsync(Guid key, EquipConnectUpdateDto dto)
+    {
+        EquipConnectReadDto? entity = await base.UpdateAsync(key, dto);
+        if (entity == null) return entity;
+        EquipConnect equipConnect = await Queryable.FirstAsync(x => x.Id == key);
+        if (equipConnect == null) return entity;
+        await PutStartConnect(entity.Id);
+        return entity;
+    }
+
+    /// <summary>
+    /// 启动连接
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    public async Task PutStartConnect(Guid id)
+    {
+        EquipConnect connect = await Queryable.Where(it => it.Id == id)
+           .Includes(t => t.EquipLedger)
+           .FirstAsync();
+        IotTopicBuilder iotTopicBuilder = IotTopicBuilder.CreateIotBuilder()
+                .WithPrefix(TopicType.Iot)
+                .WithDirection(MqttDirection.Down)
+                .WithTag(MqttTag.Cmd)
+                .WithDeviceType(connect.ProtocolEnum.ToString())
+                .WithUri(connect.EquipId.ToString());
+
+        ConnInfo<SocketConnInfo> info = new ConnInfo<SocketConnInfo>();
+        info.ConnType = ConnType.Scoket;
+        info.Content = new SocketConnInfo() { Address = "192.168.110.94", Port = 502 };
+        info.Type = CmdType.Conn;
+        string socketDto = JsonSerializer.Serialize(info);
+        byte[] msg = Encoding.UTF8.GetBytes(socketDto);
+        string topic = iotTopicBuilder.Build();
+        if (await _mqttExplorer.IsConnectedAsync())
+        {
+            await _mqttExplorer.PublishAsync(topic, msg);
+        }
+    }
+
+    /// <summary>
+    /// 关闭连接
+    /// </summary>
+    /// <param name="connectId">配置Id</param>
+    /// <returns></returns>
+    public async Task StopConnectAsync(Guid connectId)
+    {
+        IotTopicBuilder iotTopicBuilder = IotTopicBuilder.CreateIotBuilder()
+                .WithPrefix(TopicType.Iot)
+                .WithDirection(MqttDirection.Down)
+                .WithTag(MqttTag.Cmd)
+                .WithUri(connectId.ToString());
+        string topic = iotTopicBuilder.Build();
+        await _mqttExplorer.PublishAsync(topic, null);
+    }
+
+    /// <summary>
+    /// 测试连接
+    /// </summary>
+    /// <param name="protocolEnum"></param>
+    /// <param name="connectionString"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentOutOfRangeException"></exception>
+    public async Task TestConnection(Protocol protocolEnum, string connectionString)
+    {
+        var guid = Guid.NewGuid();
+        var connect = new EquipConnect()
+        {
+            Id = guid,
+            Name = "测试123"
+        };
+        connect.ProtocolEnum = protocolEnum;
+        await PutStartConnect(guid);
     }
 }
---
+++ src/Hgzn.Mes.WebApi/Controllers/Equip/EquipConnectController.cs
@@ -0,0 +1,89 @@
+﻿using Hgzn.Mes.Application.Main.Dtos.Equip;
+using Hgzn.Mes.Application.Main.Services.Equip;
+using Hgzn.Mes.Application.Main.Services.Equip.IService;
+using Hgzn.Mes.Domain.Shared;
+using Hgzn.Mes.Domain.ValueObjects;
+using Hgzn.Mes.WebApi.Utilities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hgzn.Mes.WebApi.Controllers.Equip
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EquipConnectController : ControllerBase
+    {
+        private readonly IEquipConnService _equipConnectService;
+        public EquipConnectController(IEquipConnService equipConnectService)
+        {
+            _equipConnectService = equipConnectService;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [Route("page")]
+        public async Task<ResponseWrapper<PaginatedList<EquipConnectReadDto>?>> GetPaginatedListAsync(EquipConnectQueryDto queryDto)
+        => (await _equipConnectService.GetPaginatedListAsync(queryDto)).Wrap()!;
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [Route("list")]
+        public async Task<ResponseWrapper<IEnumerable<EquipConnectReadDto>?>> GetListAsync(EquipConnectQueryDto queryDto)
+            => (await _equipConnectService.GetListAsync(queryDto)).Wrap()!;
+
+        /// <summary>
+        /// 创建
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("create")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [Authorize(Policy = $"system:code:{ScopeMethodType.Add}")]
+        public async Task<ResponseWrapper<EquipConnectReadDto?>> CreateAsync(EquipConnectCreateDto input) =>
+            (await _equipConnectService.CreateAsync(input)).Wrap()!;
+
+        /// <summary>
+        /// 删除
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpDelete]
+        [Route("{id:guid}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [Authorize(Policy = $"system:code:{ScopeMethodType.Remove}")]
+        public async Task<ResponseWrapper<int>> DeleteAsync(Guid id) =>
+            (await _equipConnectService.DeleteAsync(id)).Wrap();
+
+        /// <summary>
+        /// 更新
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        [HttpPut]
+        [Route("{id:guid}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [Authorize(Policy = $"system:code:{ScopeMethodType.Edit}")]
+        public async Task<ResponseWrapper<EquipConnectReadDto?>> UpdateAsync(Guid id, EquipConnectUpdateDto input) =>
+            (await _equipConnectService.UpdateAsync(id, input)).Wrap()!;
+
+        /// <summary>
+        /// 获取
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("{id:guid}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [Authorize(Policy = $"system:code:{ScopeMethodType.Query}")]
+        public async Task<ResponseWrapper<EquipConnectReadDto?>> GetAsync(Guid id) =>
+            (await _equipConnectService.GetAsync(id)).Wrap()!;
+    }
+}
