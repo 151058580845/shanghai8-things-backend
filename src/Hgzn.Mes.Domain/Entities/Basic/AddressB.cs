9c33095c83a823b3ca7a142b246e097c44a4d962
zxj <1186659799@qq.com>
2025-02-24 09:03:05 +0800

    feat: 增加基础管理的实体

---
+++ src/Hgzn.Mes.Domain/Entities/Basic/AddressB.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using Hgzn.Mes.Domain.Entities.Base;
+using Hgzn.Mes.Domain.Entities.Base.Audited;
+
+namespace Hgzn.Mes.Domain.Entities.Basic;
+
+[Description("地址管理")]
+public class AddressB: UniversalEntity, IAudited
+{
+    public Guid? CreatorId { get; set; }
+    public DateTime CreationTime { get; set; }
+    public Guid? LastModifierId { get; set; }
+    public DateTime? LastModificationTime { get; set; }
+    
+    /// <summary>
+    /// 父类主键
+    /// </summary>
+    public Guid ParentId { get; set; }
+
+    /// <summary>
+    /// 联系人名称
+    /// </summary>
+    public string Address { get; set; } = null!;
+}
---
+++ src/Hgzn.Mes.Domain/Entities/Basic/Customer.cs
@@ -0,0 +1,56 @@
+﻿using System.ComponentModel;
+using Hgzn.Mes.Domain.Entities.Base;
+using Hgzn.Mes.Domain.Entities.Base.Audited;
+
+namespace Hgzn.Mes.Domain.Entities.Basic;
+
+[Description("客户管理")]
+public class Customer: UniversalEntity, IAudited
+{
+    public Guid? CreatorId { get; set; }
+    public DateTime CreationTime { get; set; }
+    public Guid? LastModifierId { get; set; }
+    public DateTime? LastModificationTime { get; set; }
+    
+    [Description("客户编码")]
+    public string Code { get; set; } = null!;
+    
+    [Description("客户名称")]
+    public string Name { get; set; } = null!;
+
+    [Description("结算方式")]
+    public string SettlementMethod { get; set; } = null!;
+    
+    [Description("是否包含税")]
+    public bool TaxIncluded { get; set; }
+    
+    [Description("是否包含运费")]
+    public bool FreightIncluded { get; set; }
+    
+    [Description("客户简称")]
+    public string? ShortName { get; set; }
+    
+    [Description("客户英文名称")]
+    public string? EnglishName { get; set; }
+    
+    [Description("客户介绍")]
+    public string? Description { get; set; }
+    
+    [Description("客户官网地址")]
+    public string? Website { get; set; }
+    
+    [Description("客户邮箱")]
+    public string? Email { get; set; }
+    
+    [Description("客户电话")]
+    public string? Phone { get; set; }
+    
+    [Description("社会信用代码")]
+    public string? SocialCreditCode { get; set; }
+    
+    [Description("备注")]
+    public string? Remark { get; set; }
+    
+    public List<Contact>? Contacts { get; set; }
+    public List<AddressB>? AddressBs { get; set; }
+}
