7b402741d81be9d027eeafd6d8680e462b450a59
leaf3woods <leaf3woods@gmail.com>
2025-06-06 15:12:44 +0800

    fix: 创建和更新接口问题修复

--- src/Hgzn.Mes.Application.Main/Dtos/System/FloorDto.cs
+++ src/Hgzn.Mes.Application.Main/Dtos/System/FloorDto.cs
@@ -94,7 +94,7 @@ public class FloorUpdateDto : UpdateDto
     /// <summary>
     /// 楼层面积（平方米）
     /// </summary>
-    public decimal Area { get; set; }
+    public decimal? Area { get; set; }
 
     /// <summary>
     /// 房间数量
